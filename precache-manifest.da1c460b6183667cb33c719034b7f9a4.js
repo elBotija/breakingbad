8b3181d59e184e0ce367db778e6913733c07d6de
Mauro Martinez <mmartinez@edrans.com>
2019-09-19 19:23:25 -0300

    Updates

---
+++ precache-manifest.da1c460b6183667cb33c719034b7f9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679fa1dc23924052e6bdbc3c32acac39",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "ec0a997b663aefd10310",
+    "url": "/breakingbad/static/css/main.211b9b7c.chunk.css"
+  },
+  {
+    "revision": "f18d2631880e1e08ecf8",
+    "url": "/breakingbad/static/js/2.cede1786.chunk.js"
+  },
+  {
+    "revision": "ec0a997b663aefd10310",
+    "url": "/breakingbad/static/js/main.c49fd23a.chunk.js"
+  },
+  {
+    "revision": "c0e079704eb7fdb9d2a4",
+    "url": "/breakingbad/static/js/runtime-main.3fc68e8d.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breakingbad/static/media/logo.427a4adc.svg"
+  }
+]);

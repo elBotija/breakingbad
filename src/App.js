b4d6a7f7346f4cb6c3008743ca0c0fbe8b23c5fa
Mauro Martinez <mmartinez@edrans.com>
2019-09-19 19:18:43 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Frase({frase}){
+  if(frase && frase.quote){
+    return(
+      <div className="frase">
+        <h1>{frase.quote}</h1>
+        <p>-{frase.author}</p>
+      </div>
+    )
+  }else{
+    return "Loading..."
+  }
 }
 
-export default App;
+function App(){
+
+  const [frase, obtenerFrase] = useState({})
+
+  const consultarAPI = async () => {
+    const resultado = await axios('http://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    obtenerFrase(resultado.data[0])
+  }
+
+  useEffect(
+    ()=>{
+      consultarAPI()
+    },[]
+  )
+
+  console.log(frase)
+
+  return (
+    <div className="contenedor">
+      <Frase frase={frase}/>
+      <button onClick={consultarAPI}>Generar Nueva Frase</button>
+    </div>
+  )
+}
+
+export default App
